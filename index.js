fe04378640510ded74b6162922751467c9b89759
ehsan2793 <ehsan2793@gmail.com>
2021-10-21 18:01:28 -0700

    axios call working

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const port = 5000
+const port = 5000;
 
-const server = express()
-
+const server = express();
 
 server.get('/', (req, res) => {
-    res.status(200).json("Welcome to Coronavirus News API")
-})
+    res.status(200).json('Welcome to Coronavirus News API');
+});
+server.get('/news', (req, res) => {
+    axios
+        .get('https://www.theguardian.com/world/coronavirus-outbreak')
+        .then((res) => {
+            const data = res.data;
+            const $ = cheerio.load(data);
+            $('a:contains("coronavirus)', data).each(function () {
+
+            })
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 server.listen(port, function () {
-
-    console.log('Server running on port %s', port)
-})
+    console.log('Server running on port %s', port);
+});
