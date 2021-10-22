dd546a085d246edea7c4d385f72d0039fa8529d9
ehsan2793 <ehsan2793@gmail.com>
2021-10-21 21:58:28 -0700

    chnage

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const cors = require('cors');
 const helmet = require('helmet');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 const server = express();
 server.use(cors());
