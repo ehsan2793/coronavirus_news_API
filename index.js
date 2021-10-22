f55f6a3091ca6460d9410d555ce74111c1045bed
ehsan2793 <ehsan2793@gmail.com>
2021-10-21 18:30:05 -0700

    cors and helmet installed

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cors = require('cors')
+const helmet = require('helmet')
 const port = 5000;
 
-const server = express();
 
+const server = express();
+server.use(cors());
+server.use(helmet());
+const news = []
 server.get('/', (req, res) => {
     res.status(200).json('Welcome to Coronavirus News API');
 });
 server.get('/news', (req, res) => {
     axios
         .get('https://www.theguardian.com/world/coronavirus-outbreak')
-        .then((res) => {
-            const data = res.data;
+        .then((response) => {
+            const data = response.data;
             const $ = cheerio.load(data);
-            $('a:contains("coronavirus)', data).each(function () {
-
-            })
+            $('a:contains("coronavirus")', data).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href');
+                news.push({
+                    title,
+                    url,
+                })
+            });
+            res.status(200).json(news)
         })
         .catch((err) => {
             console.log(err);
